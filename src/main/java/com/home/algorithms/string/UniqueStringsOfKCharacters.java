cdd91ddd399205c0fb34e07278e6688662852eab
Andrey Yun <andrey_yun@epam.com>
2019-11-28 00:26:52 -0500

    fixed algorithm

--- src/main/java/com/home/algorithms/string/UniqueStringsOfKCharacters.java
+++ src/main/java/com/home/algorithms/string/UniqueStringsOfKCharacters.java
@@ -1,31 +1,17 @@
 package com.home.algorithms.string;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UniqueStringsOfKCharacters {
 
-    public List<String> bruteForce(char[] input, int numberOfChars) {
-        List<String> res = new ArrayList<>();
-        for (int i=0; i<input.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j=0; j<numberOfChars; j++) {
-                int idx = j!=0 ? i % j : i;
-                sb.append(input[idx]);
-                for (int k=0; k<3; k++) {
-                    int idx2 = k!=0 ? i % k : i;
-                    sb.append(input[idx2]);
-                    res.add(sb.toString());
-                }
-            }
+    void find(char[] inputArray, int k, String prefix, List<String> collect) {
+        if (k == 0) {
+            collect.add(prefix);
+            return;
         }
-        return res;
-    }
-
-    private int mod(int i) {
-        if (i < 0) {
-            return -1*i;
+        for (int i = 0; i < inputArray.length; ++i) {
+            String newPrefix = prefix + inputArray[i];
+            find(inputArray, k - 1, newPrefix, collect);
         }
-        return i;
     }
 }
--- src/test/groovy/com/home/algorithms/string/UniqueStringsOfKCharactersTest.groovy
+++ src/test/groovy/com/home/algorithms/string/UniqueStringsOfKCharactersTest.groovy
@@ -6,13 +6,14 @@ class UniqueStringsOfKCharactersTest extends Specification {
 
     UniqueStringsOfKCharacters uniqueStringsOfKCharacters = new UniqueStringsOfKCharacters()
 
-    def 'test bruteforce'() {
+    def 'test recursive with loop'() {
         given: 'array and length of unique Strings k'
         def inputArray = ['a', 'b'] as char[]
         int k = 3
 
         when: 'run method'
-        def res = uniqueStringsOfKCharacters.bruteForce(inputArray, k)
+        def res = []
+        uniqueStringsOfKCharacters.find(inputArray, k, "", res)
 
         then: '8 unique combinations'
         res.sort({ a, b -> (a <=> b) }) == ['aaa','aab','aba','abb','baa','bab','bba','bbb']
