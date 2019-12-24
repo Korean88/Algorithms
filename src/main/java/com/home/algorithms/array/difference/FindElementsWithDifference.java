4454d1a4055cf46c2fc7ff51da4418d94041d27e
Korean88 <andrey_yun@mail.ru>
2019-12-24 11:09:55 -0500

    added an optimized solution (#4)

--- src/main/java/com/home/algorithms/array/difference/FindElementsWithDifference.java
+++ src/main/java/com/home/algorithms/array/difference/FindElementsWithDifference.java
@@ -2,6 +2,7 @@ package com.home.algorithms.array.difference;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,6 +14,24 @@ public class FindElementsWithDifference {
             for (int j = i+1; j < array.length; j++) {
                 if (array[i] - array[j] == difference || array[j] - array[i] == difference) {
                     res.add(Pair.of(array[i], array[j]));
+                }
+            }
+        }
+        return res;
+    }
+
+    Set<Pair<Integer, Integer>> findWithSorting(int[] array, int difference) {
+        Set<Pair<Integer, Integer>> res = new HashSet<>();
+        if (array == null || array.length < 2) {
+            return res;
+        }
+        Arrays.sort(array);
+        for (int i=0; i<array.length-1; i++) {
+            int j=i;
+            while (++j<array.length && array[j]-array[i]<=difference) {
+                if (array[j]-array[i]==difference) {
+                    res.add(Pair.of(array[i], array[j]));
+                    break;
                 }
             }
         }
--- src/test/groovy/com/home/algorithms/array/difference/FindElementsWithDifferenceTest.groovy
+++ src/test/groovy/com/home/algorithms/array/difference/FindElementsWithDifferenceTest.groovy
@@ -10,12 +10,31 @@ class FindElementsWithDifferenceTest extends Specification {
     def 'bruteforce should find all pairs with difference 2'() {
         given: 'input and expected result'
         def input = [1, 7, 5, 9, 2, 12, 3] as int[]
-        def expected = new HashSet([Pair.of(1,3), Pair.of(7,5), Pair.of(7,9), Pair.of(5,3)])
+        def expected = new HashSet([Pair.of(1, 3), Pair.of(7, 5), Pair.of(7, 9), Pair.of(5, 3)])
 
         when: 'find all pairs'
         def res = findElementsWithDifference.findPairWithDifferenceBruteForce(input, 2)
 
         then: 'compare result with expected'
         res == expected
+    }
+
+    def 'findWithSorting should find all pairs with given difference'() {
+        given: 'input and expected result'
+        def input = inputArray as int[]
+
+        when: 'find all pairs'
+        def res = findElementsWithDifference.findWithSorting(input, givenDifference)
+
+        then: 'compare result with expected'
+        res == expected as Set
+
+        where: 'parameters'
+        inputArray             | givenDifference | expected
+        [1, 7, 5, 9, 2, 12, 3] | 2               | [Pair.of(1, 3), Pair.of(5, 7), Pair.of(3, 5), Pair.of(7, 9)]
+        [1, 7]                 | 6               | [Pair.of(1, 7)]
+        [1]                    | 6               | []
+        []                     | 6               | []
+        [1, 7, 5, 9, 2, 12, 3] | 60              | []
     }
 }
