8bae9a604dfdeec69b0e5e5c4e4c03a9dfef0472
Andrey Yun <Andrey_Yun@epam.com>
2019-12-24 12:35:25 -0500

    stack sequence validation implemented

--- src/main/java/com/home/algorithms/leetcode/stack/ValidateStackSequence.java
+++ src/main/java/com/home/algorithms/leetcode/stack/ValidateStackSequence.java
@@ -11,16 +11,14 @@ public class ValidateStackSequence {
 
         Stack<Integer> tempStack = new Stack<>();
         int poppedIdx = 0;
-        for (int i=0; i<pushed.length; i++) {
+        for (int i = 0; i < pushed.length; i++) {
             tempStack.push(pushed[i]);
             if (tempStack.peek() == popped[poppedIdx]) {
                 tempStack.pop();
                 poppedIdx = checkPrevious(tempStack, popped, ++poppedIdx);
-            } else if (poppedIdx == popped.length - 1) {
-                return false;
             }
         }
-        return true;
+        return tempStack.isEmpty();
     }
 
     private int checkPrevious(Stack<Integer> tempStack, int[] popped, int poppedIdx) {
