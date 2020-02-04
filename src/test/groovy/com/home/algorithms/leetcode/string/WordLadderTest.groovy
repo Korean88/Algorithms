951621e29a98e22484a088c510cf25265459d8e6
Andrey Yun <andrey_yun@epam.com>
2020-02-04 00:38:17 -0500

    bfs algorithm

---
+++ src/test/groovy/com/home/algorithms/leetcode/string/WordLadderTest.groovy
@@ -0,0 +1,22 @@
+package com.home.algorithms.leetcode.string
+
+import spock.lang.Specification
+
+class WordLadderTest extends Specification {
+
+    WordLadder wordLadder = new WordLadder()
+
+    def 'test word ladder'() {
+        given: 'parameterized test'
+        when: 'method called'
+        def res = wordLadder.ladderLength(beginWord, endWord, wordList as List<String>)
+
+        then: 'verify result'
+        res == expected
+
+        where: 'parameters'
+        beginWord | endWord | wordList                                   | expected
+        "hit"     | "cog"   | ["hot", "dot", "dog", "lot", "log", "cog"] | 5
+        "hit"     | "hot"   | ["hot", "dot", "dog", "lot", "log", "cog"] | 2
+    }
+}
