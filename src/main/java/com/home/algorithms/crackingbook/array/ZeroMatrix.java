5ffbb1b7fd486a1247bbbe302bda527f01fa8481
Andrey Yun <andrey_yun@epam.com>
2020-02-06 20:49:18 -0500

    zero matrix refactor

--- src/main/java/com/home/algorithms/crackingbook/array/ZeroMatrix.java
+++ src/main/java/com/home/algorithms/crackingbook/array/ZeroMatrix.java
@@ -3,35 +3,25 @@ package com.home.algorithms.crackingbook.array;
 public class ZeroMatrix {
 
     int[][] nullifyMatrix(int[][] matrix) {
-        if (matrix == null) {
-            return new int[][]{};
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return matrix;
         }
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        boolean[] zeroRowIndexes = new boolean[rows];
-        boolean[] zeroColIndexes = new boolean[cols];
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<cols; j++) {
-                if (matrix[i][j]==0) {
-                    zeroRowIndexes[i] = true;
-                    zeroColIndexes[j] = true;
+        boolean[] zeroRows = new boolean[matrix.length];
+        boolean[] zeroCols = new boolean[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
                 }
             }
         }
-        for (int i=0; i<zeroRowIndexes.length; i++) {
-            if (zeroRowIndexes[i]) {
-                for (int j=0; j<cols; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for (int i=0; i<zeroColIndexes.length; i++) {
-            if(zeroColIndexes[i]) {
-                for (int j=0; j< rows; j++) {
-                    matrix[j][i]=0;
-                }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (zeroRows[i] || zeroCols[j]) matrix[i][j] = 0;
             }
         }
         return matrix;
     }
+
 }
